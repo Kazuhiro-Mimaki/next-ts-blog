70f80f014754d2061011ebdc0df53b35de1d65e1
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-02-11 16:39:15 +0900

    Add home button

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -4,20 +4,26 @@ import Link from "next/link";
 const HeaderComponent = () => {
   return (
     <header className={styles.header}>
-      <Link href="/posts/tech">
-        <a className={styles.item}>Tech</a>
+      <Link href="/">
+        <a className={`${styles.item} ${styles.title}`}>Kazuhiro Mimaki</a>
       </Link>
-      <Link href="/posts/life">
-        <a className={styles.item}>Life</a>
-      </Link>
-      <Link href="/posts/self-reflection">
-        <a className={styles.item}>Self-Reflection</a>
-      </Link>
-      <Link href="https://github.com/Kazuhiro-Mimaki">
-        <a className={styles.item} target="_blank" rel="noopener noreferrer">
-          GitHub
-        </a>
-      </Link>
+
+      <nav className={styles.nav}>
+        <Link href="/posts/tech">
+          <a className={styles.item}>Tech</a>
+        </Link>
+        <Link href="/posts/life">
+          <a className={styles.item}>Life</a>
+        </Link>
+        <Link href="/posts/self-reflection">
+          <a className={styles.item}>Self-Reflection</a>
+        </Link>
+        <Link href="https://github.com/Kazuhiro-Mimaki">
+          <a className={styles.item} target="_blank" rel="noopener noreferrer">
+            GitHub
+          </a>
+        </Link>
+      </nav>
     </header>
   );
 };
