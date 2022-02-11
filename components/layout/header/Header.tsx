960f39cebf015e1a35a5dcf15d62d8944425c12a
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-02-11 13:56:21 +0900

    Add item to header nav

--- components/layout/header/Header.tsx
+++ components/layout/header/Header.tsx
@@ -13,6 +13,14 @@ const HeaderComponent = () => {
       <Link href="/posts/self-reflection">
         <a className={styles.item}>Self-Reflection</a>
       </Link>
+      <Link href="https://github.com/Kazuhiro-Mimaki">
+        <a className={styles.item} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+      </Link>
+      <Link href="/posts/resume">
+        <a className={styles.item}>Resume</a>
+      </Link>
     </header>
   );
 };
