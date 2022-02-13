561dc7a3beece5913e3fd2d94d35384b45437413
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-02-14 00:09:21 +0900

    Remove rss parser

--- components/componentProvider.ts
+++ components/componentProvider.ts
@@ -2,7 +2,6 @@ import HeaderComponent from "./layout/header/Header";
 import FooterComponent from "./layout/footer/Footer";
 import PostComponent from "./post/Post";
 import LogoComponent from "./logo/Logo";
-import LinkButtonComponent from "./button/link-button/LinkButton";
 import LayoutComponent from "./layout/Layout";
 import MarkdownComponent from "./markdown/Markdown";
 import PageComponent from "./layout/page/Page";
@@ -13,7 +12,6 @@ export {
   FooterComponent,
   PostComponent,
   LogoComponent,
-  LinkButtonComponent,
   LayoutComponent,
   MarkdownComponent,
   PageComponent,
