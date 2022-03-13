8b46511f90bb8c6258223b7b2a1c5de060821fd6
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-03-13 10:26:39 +0900

    Post for reflection

---
+++ pages/reflection/index.tsx
@@ -0,0 +1,37 @@
+import Post from "../../types/post/post";
+import notePosts from "../../_tech/_note/posts.json";
+import style from "./reflection.module.css";
+
+const Index = () => {
+  return (
+    <>
+      <div className={style.container}>
+        <div className={style["section-title"]}>
+          <h2 className={style.title}>Reflection</h2>
+          <p className={style.memo}>月次振り返り</p>
+        </div>
+        <div className={style.main}>
+          {filterdPostList(notePosts).map((post, index) => {
+            return (
+              <a
+                className={style.card}
+                href={post.link}
+                key={index}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <p className={style["post-title"]}>{post.title}</p>
+              </a>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const filterdPostList = (postList: Post[]) => {
+  return postList.filter((post) => post.title.includes("Monthly"));
+};
+
+export default Index;
