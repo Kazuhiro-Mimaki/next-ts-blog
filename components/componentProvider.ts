b0e793ac5afa98049616d19ce197bcf1fe3d0b25
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2022-03-13 21:34:37 +0900

    Remove redundant

--- components/componentProvider.ts
+++ components/componentProvider.ts
@@ -1,19 +1,15 @@
 import HeaderComponent from "./layout/header/Header";
 import FooterComponent from "./layout/footer/Footer";
-import PostComponent from "./post/Post";
 import LogoComponent from "./logo/Logo";
 import LayoutComponent from "./layout/Layout";
 import MarkdownComponent from "./markdown/Markdown";
-import PageComponent from "./layout/page/Page";
 import DefinitionItemComponent from "./item/Item";
 
 export {
   HeaderComponent,
   FooterComponent,
-  PostComponent,
   LogoComponent,
   LayoutComponent,
   MarkdownComponent,
-  PageComponent,
   DefinitionItemComponent,
 };
